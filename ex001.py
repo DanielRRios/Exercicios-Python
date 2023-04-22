6d1b314b1d93ea9e6ac1cac4da4d3a98340f21f4
DanielRRios <daniel.rios@sptech.school>
2023-04-22 01:14:28 -0300

    oi

--- ex001.py
+++
@@ -1,3 +0,0 @@
-# Exercício Python 001: Crie um prorama que escreva "Olá, mundo." na tela.
-
-print('Olá, mundo.')
--- ex006.py
+++
@@ -1,4 +0,0 @@
-# Exercício Python 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
-
-num = int(input('Digite um número : '))
-print(f'O dobro de {num} vale {num * 2} \nO triplo de {num} vale {num * 3} \nA raiz quadrada de {num} é igual a {num ** 0.5:.2f}.')
