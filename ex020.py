d4fee20e9c6cd38ee1f6dec16b9da54f767dd5ed
Daniel Rios <125910742+DanielRRios@users.noreply.github.com>
2023-06-14 21:05:19 -0300

    Create ex020.py

---
+++ ex020.py
@@ -0,0 +1,13 @@
+# Exercício Python 020: O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa
+# que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+import random 
+
+n1 = input('Primeiro aluno : ')
+n2 = input('Segundo aluno : ')
+n3 = input('Terceiro aluno : ')
+n4 = input('Quarto aluno : ')
+alunos = [n1, n2, n3, n4]
+random.shuffle(alunos)
+print(alunos)
+
